b25d32ec392656c367494706bc57295e23b2c48f
Tobi Maulana <tobimaulana.tm@gmail.com>
2023-01-12 11:19:56 +0700

    Configure router biaya_pmb in app.js
    biayapmbRouter

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var angkatanRouter = require('./routes/mahasiswa');
 var biayalainRouter = require('./routes/biaya_lain');
+var biayapmbRouter = require('./routes/biaya_pmb');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/angkatan', angkatanRouter);
 app.use('/biaya_lain', biayalainRouter);
+app.use('/biaya_pmb', biayapmbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
