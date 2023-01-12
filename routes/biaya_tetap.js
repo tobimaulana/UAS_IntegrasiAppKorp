4c34a73e7520a00e146611e474abb933f22d0051
Tobi Maulana <tobimaulana.tm@gmail.com>
2023-01-12 11:23:13 +0700

    Add routes biaya_tetap.js

---
+++ routes/biaya_tetap.js
@@ -0,0 +1,81 @@
+var express = require('express'); 
+const cekToken = require('../middleware.js');
+var router = express.Router(); 
+
+//panggil Model biaya_tetap
+var Biaya_Tetap = require('../models/Biaya_Tetap.js');
+
+/* TAMPIL DATA */ 
+router.get('/', cekToken, function(req, res, next) { 
+    Biaya_Tetap.findAll().then( data=>{ 
+        res.json({ 
+            status:true, 
+            pesan:"Berhasil Tampil", 
+            data:data 
+        }); 
+    }).catch(err => { 
+        res.json({ 
+            status:false, 
+            pesan:"Gagal Tampil: " + err.message, 
+            data:[] 
+        }); 
+    }); 
+});
+
+/* TAMBAH DATA */ 
+router.post('/', cekToken, function(req, res, next) { 
+    Biaya_Tetap.create(req.body).then (data=>{ 
+        res.json({ 
+            status:true, 
+            pesan:"Berhasil Tambah", 
+            data:data 
+        }); 
+    }) 
+    .catch(err => { 
+        res.json({ 
+            status:false, 
+            pesan:"Gagal Tambah: " + err.message, 
+            data:[] 
+        }); 
+    }); 
+}); 
+/* UBAH DATA */ 
+router.put('/', cekToken, function(req, res, next) { 
+    Biaya_Tetap.update(req.body,{ 
+        where:{id:req.body.id} 
+    }).then ( ()=>{ 
+        res.json({ 
+            status:true, 
+            pesan:"Berhasil Ubah", 
+            data: [] 
+        }); 
+    }) 
+    .catch(err => { 
+        res.json({ 
+            status:false, 
+            pesan:"Gagal Ubah: " + err.message, 
+            data:[] 
+        }); 
+    }); 
+}); 
+/* HAPUS DATA */ 
+router.delete('/', cekToken, function(req, res, next) { 
+    Biaya_Tetap.destroy({ 
+        where:{id:req.body.id} 
+    }).then ( ()=>{ 
+        res.json({ 
+            status:true, 
+            pesan:"Berhasil Hapus", 
+            data: [] 
+        }); 
+    }) 
+    .catch(err => { 
+        res.json({ 
+            status:false, 
+            pesan:"Gagal Hapus: " + err.message, 
+            data:[] 
+        }); 
+    }); 
+}); 
+
+module.exports = router;
