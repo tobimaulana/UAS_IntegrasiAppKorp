4c43914576b19a480835e16566e04cf0bd2de253
Tobi Maulana <tobimaulana.tm@gmail.com>
2023-01-12 18:04:26 +0700

    Add cekToken function from middleware.js

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Users = require("../models/Users");
 var jwt = require('jsonwebtoken');
+var cekToken = require("../middleware");
 
 /* TAMPIL DATA USERS */
-router.get('/', function(req, res, next) {
+router.get('/', cekToken, function(req, res, next) {
   Users.findAndCountAll().then( data => {
          res.json({
                 status:true,
@@ -22,7 +23,7 @@ router.get('/', function(req, res, next) {
 
 
 /* TAMBAH DATA USERS */
-router.post('/', function(req, res, next) {
+router.post('/', cekToken, function(req, res, next) {
   Users.create(req.body).then (data => {
          res.json({
                 status:true,
@@ -41,7 +42,7 @@ router.post('/', function(req, res, next) {
 
 
 /* UBAH DATA USERS */
-router.put('/', function(req, res, next) {
+router.put('/', cekToken, function(req, res, next) {
   Users.update(req.body, {
          where:{id:req.body.id}
   }).then( () => {
@@ -62,7 +63,7 @@ router.put('/', function(req, res, next) {
 
 
 /* HAPUS DATA USERS */
-router.delete('/', function(req, res, next) {
+router.delete('/', cekToken, function(req, res, next) {
   Users.destroy({
          where:{id:req.body.id}
   }).then ( () => {
