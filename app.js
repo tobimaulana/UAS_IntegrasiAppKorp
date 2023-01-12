49c334570e599ffbad8dab19abcf3a3dd152b1e9
Tobi Maulana <tobimaulana.tm@gmail.com>
2023-01-12 11:12:51 +0700

    Configure a router routes/biaya_lain.js
    biayalainRouter

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var angkatanRouter = require('./routes/mahasiswa');
+var biayalainRouter = require('./routes/biaya_lain');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/angkatan', angkatanRouter);
+app.use('/biaya_lain', biayalainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
